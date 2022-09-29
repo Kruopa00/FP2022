ccaf9932e9828de59c37e3a794783e9dbf70628a
Rokas Čebatorius <77296454+Rokyzas@users.noreply.github.com>
2022-09-30 01:44:32 +0300

    Toggle funkcija

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -14,6 +14,7 @@ import Types
 data State = State [(String, Document)]
     deriving Show
 
+
 -- IMPLEMENT
 -- This is very initial state of your program
 emptyState :: State
@@ -26,23 +27,49 @@ gameStart :: State -> Document -> State
 gameStart (State l) d = State $ ("Game", DList [DMap [("occupied_cells", DList []), ("hints", DList [])], d ]) : l
 
 
+
 -- IMPLEMENT
 -- renders your game board
 render :: State -> String
 render a = show a
 
- 
+
 -- IMPLEMENT
 -- Make check from current state
 mkCheck :: State -> Check
 mkCheck _ = Check []
 
+
 -- IMPLEMENT
 -- Toggle state's value
 -- Receive raw user input tokens
 toggle :: State -> [String] -> State
---toggle (State l) t = State $ ("Toggle " ++ show t, DNull) : l
-toggle (State (l1,[[(l2,ls)]])) (x:y:[]) = 
+toggle (State (l:ls)) t = State ((func1 l t) : ls)
+
+func1 :: (String, Document) -> [String] -> (String, Document)
+func1 (l,ls) t = (l, func2 ls t)
+
+func2 :: Document -> [String] -> Document
+func2 (DList (l:ls)) t = DList ((func3 l t):ls)
+
+func3 :: Document -> [String] -> Document
+func3 (DMap (l:ls)) t = DMap (func4 l t :ls)
+
+func4 :: (String, Document) -> [String] -> (String, Document)
+func4 (l, ls) t = (l, func5 ls t)
+
+func5 :: Document -> [String] -> Document
+func5 (DList l) t = (DList (func6 l t))
+
+func6 :: [Document] -> [String] -> [Document]
+func6 l (x:y:[]) = DMap ([("col", DInteger (read x)), ("row",DInteger (read y))]):l
+
+
+
+
+
+
+
     --if(l == "occupied_cells")
         --thenW
             --DMap ([("col", DInteger (read x)), ("row",DInteger (read y))]) : ls
@@ -61,4 +88,4 @@ toggle (State (l1,[[(l2,ls)]])) (x:y:[]) =
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
-hint (State l) h = State $ ("Hint ", h) : l
+hint (State l) h = State $ ("Hint " ++ show h, DNull) : l
