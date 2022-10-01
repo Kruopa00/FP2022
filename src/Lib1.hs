102dcc1299fdb888ba263fccd354cbfb071b4372
Rokas Čebatorius <77296454+Rokyzas@users.noreply.github.com>
2022-10-01 21:42:26 +0300

    working version

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -14,6 +14,18 @@ import Types
 data State = State [(String, Document)]
     deriving Show
 
+-- GAME MAP
+mapas :: [((Int, Int),Int)]
+mapas = [( (1,1),0),((1,2),0),((1,3),0),((1,4),0),((1,5),0),((1,6),0),((1,7),0),((1,8),0),((1,9),0),((1,10),0),
+         ((2,1),0),((2,2),0),((2,3),0),((2,4),0),((2,5),0),((2,6),0),((2,7),0),((2,8),0),((2,9),0),((2,10),0),
+         ((3,1),0),((3,2),0),((3,3),0),((3,4),0),((3,5),0),((3,6),0),((3,7),0),((3,8),0),((3,9),0),((3,10),0),
+         ((4,1),0),((4,2),0),((4,3),0),((4,4),0),((4,5),0),((4,6),0),((4,7),0),((4,8),0),((4,9),0),((4,10),0),
+         ((5,1),0),((5,2),0),((5,3),0),((5,4),0),((5,5),0),((5,6),0),((5,7),0),((5,8),0),((5,9),0),((5,10),0),
+         ((6,1),0),((6,2),0),((6,3),0),((6,4),0),((6,5),0),((6,6),0),((6,7),0),((6,8),0),((6,9),0),((6,10),0),
+         ((7,1),0),((7,2),0),((7,3),0),((7,4),0),((7,5),0),((7,6),0),((7,7),0),((7,8),0),((7,9),0),((7,10),0),
+         ((8,1),0),((8,2),0),((8,3),0),((8,4),0),((8,5),0),((8,6),0),((8,7),0),((8,8),0),((8,9),0),((8,10),0),
+         ((9,1),0),((9,2),0),((9,3),0),((9,4),0),((9,5),0),((9,6),0),((9,7),0),((9,8),0),((9,9),0),((9,10),0),
+         ((10,1),0),((10,2),0),((10,3),0),((10,4),0),((10,5),0),((10,6),0),((10,7),0),((10,8),0),((10,9),0),((10,10),0)]
 
 -- IMPLEMENT
 -- This is very initial state of your program
@@ -27,18 +39,8 @@ gameStart :: State -> Document -> State
 gameStart (State l) d = State $ ("Game", DList [DMap [("occupied_cells", DList []), ("hints", DList [])], d ]) : l
 
 
-mapas :: [((Int, Int),Int)]
-mapas = [( (1,1),0),((1,2),0),((1,3),0),((1,4),0),((1,5),0),((1,6),0),((1,7),0),((1,8),0),((1,9),0),((1,10),0),
-         ((2,1),0),((2,2),0),((2,3),0),((2,4),0),((2,5),0),((2,6),0),((2,7),0),((2,8),0),((2,9),0),((2,10),0),
-         ((3,1),0),((3,2),0),((3,3),0),((3,4),0),((3,5),0),((3,6),0),((3,7),0),((3,8),0),((3,9),0),((3,10),0),
-         ((4,1),0),((4,2),0),((4,3),0),((4,4),0),((4,5),0),((4,6),0),((4,7),0),((4,8),0),((4,9),0),((4,10),0),
-         ((5,1),0),((5,2),0),((5,3),0),((5,4),0),((5,5),0),((5,6),0),((5,7),0),((5,8),0),((5,9),0),((5,10),0),
-         ((6,1),0),((6,2),0),((6,3),0),((6,4),0),((6,5),0),((6,6),0),((6,7),0),((6,8),0),((6,9),0),((6,10),0),
-         ((7,1),0),((7,2),0),((7,3),0),((7,4),0),((7,5),0),((7,6),0),((7,7),0),((7,8),0),((7,9),0),((7,10),0),
-         ((8,1),0),((8,2),0),((8,3),0),((8,4),0),((8,5),0),((8,6),0),((8,7),0),((8,8),0),((8,9),0),((8,10),0),
-         ((9,1),0),((9,2),0),((9,3),0),((9,4),0),((9,5),0),((9,6),0),((9,7),0),((9,8),0),((9,9),0),((9,10),0),
-         ((10,1),0),((10,2),0),((10,3),0),((10,4),0),((10,5),0),((10,6),0),((10,7),0),((10,8),0),((10,9),0),((10,10),0)]
 
+--TAKES MAP VALUES
 array :: [a]
 array = []
 
@@ -48,35 +50,138 @@ map1 (((_,y), z) : xs) array
         | otherwise = ( z : array) ++ map1 xs array
 map1 _ _= []
 
--- | x == 10 && y == 10 = ( z : array) ++ map1 xs array
+
+
+n :: [((Int, Int),Int)]
+n=[]
+
+
+render :: State -> String
+render a = puttingCol (take 10 (deeper a resultArray))  ++ foo (map1 (renderF1 mapas n (getToggles a toggledCords)) array) newArray (drop 10 (deeper a resultArray))
+-- -- IMPLEMENT
+-- -- renders your game board
+-- render :: State -> String
+-- --render a = show (renderF1 a mapas n)
+-- render a = show (renderF1 mapas n (getToggles a toggledCords))
+
+-- renderF1 :: State -> [((Int, Int),Int)] -> [((Int, Int),Int)] -> [((Int, Int),Int)]
+-- renderF1 l (x:xs) n
+--     |xs /= [] = do
+--         (renderF1 l xs (((f2 x l)):n)) -- likusi mapa siuncia i pradzia kartu su nauju mapu
+--     |xs == [] = reverse ((f2 x l):n)
+
+
+-- --renderF1 (State l) (x:[]) = ((f2 x))
+
+
+-- f2 :: ((Int,Int), Int) -> State -> ((Int,Int), Int)
+-- f2 ((x,y),s) l =
+--     if x == 10 
+--         then ((x, y), 1) else ((x,y),s)
+
+renderF1 :: [((Int, Int),Int)] -> [((Int, Int),Int)] -> [(Int, Int)] -> [((Int, Int),Int)] --GAUNA SENA MAP, NAUJA MAP, TOGGLE LISTA IR GRAZINA NAUJA MAP
+renderF1  (x:xs) newMap toggles
+    |xs /= [] = do
+        (renderF1  xs (((f2 x toggles)):newMap) toggles) -- likusi mapa siuncia i pradzia kartu su nauju mapu
+    |xs == [] = reverse ((f2 x toggles):newMap)
+
+
+--renderF1 (State l) (x:[]) = ((f2 x))
+
+
+f2 :: ((Int,Int), Int) -> [(Int,Int)] -> ((Int,Int), Int)
+f2 ((x,y),s) ((cx,cy):cxs) = if ((x == cx) && (y == cy)) then ((x, y), 1) else f2 ((x,y),s) cxs
+f2 ((x,y),s) _ = ((x,y),s)
+    
+
+        
+
+-- renderF2 :: (String, Document) -> [((Int, Int),Int)] -> (String, Document)
+-- renderF2 (l,ls) t = (l, renderF3 ls t)
+
+-- renderF3 :: Document -> [((Int, Int),Int)] -> Document
+-- renderF3 (DList (l:ls)) t = DList ((renderF4 l t):ls)
+
+-- renderF4 :: Document -> [((Int, Int),Int)] -> Document
+-- renderF4 (DMap (l:ls)) t = DMap (renderF5 l t :ls)
+
+-- renderF5 :: (String, Document) -> [((Int, Int),Int)] -> (String, Document)
+-- renderF5 (l, ls) t = (l, renderF6 ls t)
+
+-- renderF6 :: Document -> [((Int, Int),Int)] -> Document
+-- renderF6 (DList l) t = (DList (func6 l t))
+-- --maps[((x,y),DInt)] -- 100 tuplu -> 100 reiksmiu
 
 -- IMPLEMENT
 -- renders your game board
-render :: State -> String
-render a = puttingCol (take 11 (deeper a resultArray)) ++ foo (map1 mapas array) newArray (drop 9 (deeper a resultArray))
+-- render :: State -> String
+-- render a = puttingCol (take 10 (deeper a resultArray))  ++ foo (map1 mapas array) newArray (drop 10 (deeper a resultArray))
 --render a = show $ deeper a resultArray
 --render a = foo $ map1 mapas array
 --maps[((x,y),DInt)] -- 100 tuplu -> 100 reiksmiu
 
+--FORMATING MAP
 newArray :: String
 newArray = ""
 
 puttingCol :: [Int] -> String
 puttingCol (x:xs) = (show x ++ " ") ++ puttingCol xs
-puttingCol (x:_) = "\n" ++ show x ++ " - "
-puttingCol _ = "\n"
-
-
+puttingCol _ = "\n____________________\n"
 
 foo :: [Int] -> String -> [Int] -> String
 foo (x:xs) newArray (y:ys)
     | x /= 10 = newArray ++ show x ++ " " ++ foo xs newArray (y:ys)
-    | otherwise = newArray ++ "\n" ++ show y ++ " - " ++ foo xs newArray ys
+    | otherwise = newArray ++  "| " ++ show y ++ "\n" ++ foo xs newArray ys
 foo _ _ _ = ""
 
 
-convert :: Document -> Int
-convert (DInteger x) = x
+
+toggledCords :: [(Int, Int)]
+toggledCords = []
+
+
+getToggles :: State -> [(Int, Int)] -> [(Int, Int)]
+getToggles (State (l:ls)) cords = gT1 l cords
+
+gT1 :: (String, Document)  -> [(Int, Int)] -> [(Int, Int)] --gauna game,DList
+gT1 (l,ls) cords = gT2 ls cords
+
+gT2 :: Document  -> [(Int, Int)] -> [(Int, Int)] --gauna Dlist
+gT2 (DList (l:_)) cords = gT3 l cords
+
+gT3 :: Document  -> [(Int, Int)] -> [(Int, Int)] --gauna DMap
+gT3 (DMap (l:_)) cords = gT4 l cords
+
+gT4 :: (String, Document)  -> [(Int,  Int)] -> [(Int, Int)] --gauna occupied cells ir DList
+gT4 (_,DList []) cords = cords
+gT4 (_,ls) cords = gT5 ls cords
+
+gT5 :: Document  -> [(Int, Int)] -> [(Int, Int)]
+gT5 (DList (x:xs)) cords
+    | xs /= [] = do      --gauna DList [cia daug listu [(col, x)(row, y)]], reikia dar ir su xs padaryt rekursija
+         gT5  (DList xs) (gT6 x cords)
+    | xs == [] = gT6 x cords
+
+
+gT6 :: Document  -> [(Int, Int)] -> [(Int, Int)]
+gT6 (DMap [(_,DInteger x),(_,DInteger y)]) cords = do
+    (x,y):cords
+gT6 _ cords = (-1,-1):cords
+
+
+-- renderF1 :: [((Int, Int),Int)] -> [((Int, Int),Int)] -> (Int,Int) -> [((Int, Int),Int)]
+-- renderF1  (x:xs) n f
+--     |xs /= [] = do
+--         (renderF1  xs (((f2 x f)):n) f) -- likusi mapa siuncia i pradzia kartu su nauju mapu
+--     |xs == [] = reverse ((f2 x f):n)
+
+--gameStart (State l) d = State $ ("Game", DList [DMap [("occupied_cells", DList []), ("hints", DList [])], d ]) : l
+
+
+    --State $ ("Toggle", DList [DMap ([("col", DInteger (read x)), ("row",DInteger (read y))])]) : l
+    --1 dmap (toggle, dlist[dint, dint])
+    --2 (x:xs) s=="toggle", dlist : naujas 
+    --3
 
 
 -- IMPLEMENT
@@ -110,7 +215,7 @@ func6 :: [Document] -> [String] -> [Document]
 func6 l (x:y:[]) = DMap ([("col", DInteger (read x)), ("row",DInteger (read y))]):l
 
 
-
+-- Get result cells
 resultArray :: [Int]
 resultArray = []
 
@@ -139,9 +244,7 @@ cols1 :: Document -> [Int] -> [Int]
 cols1 (DList (DInteger x : ls)) resultArray = cols1 (DList ls) $ x : resultArray
 cols1 _ resultArray = reverse resultArray
 
-{-cols2 :: Document -> Int
-cols2 (DInteger x) = x
-cols2 _ = -1-}
+
 -- IMPLEMENT
 -- Adds hint data to the game state
 hint :: State -> Document -> State
@@ -177,5 +280,3 @@ hintFunc9 (DMap [l,(ls,lss)]) = hintFunc8 (ls,lss)
 
 hintFunc10 :: (String, Document) -> Document
 hintFunc10 (l,ls) = ls
-
-
